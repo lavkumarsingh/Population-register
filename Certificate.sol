5120dcd51330b1389646a164b88185c4115ff344
lavkumarsingh <32202008+lavkumarsingh@users.noreply.github.com>
2020-08-07 02:05:39 +0530

    Update Certificate.sol

--- Certificate.sol
+++ Certificate.sol
@@ -1,50 +1,62 @@
 
-pragma solidity ^0.4.24;
+pragma solidity^0.5.16;
 
-contract Certification {
-    constructor() public {}
-
-    struct Certificate {
-        string date;
-        string time;
-        string pName;
-        string place;
-        string addr;
-    }
-
-    mapping(bytes32 => Certificate) public certificates;
-
-    string combi;
+contract BlockCert {
     bytes32 hash;
-
-function strConcat(string memory _a, string memory _b) public pure returns (string memory){
-    bytes memory _ba = bytes(_a);
-    bytes memory _bb = bytes(_b);
-
-    string memory abcde = new string(_ba.length + _bb.length);
-    bytes memory babcde = bytes(abcde);
-    uint k = 0;
-    for (uint i = 0; i < _ba.length; i++) babcde[k++] = _ba[i];
-    for (i = 0; i < _bb.length; i++) babcde[k++] = _bb[i];
-    return string(babcde);
-}
-
+    
+    struct certificate {
+        string organisation;
+        string name;
+        string course;
+        string score;
+        string date;
+        string roll_no;
+    }
+    
+    mapping(bytes32 => certificate) public certificates;
+    
+    function strConcat(
+        string memory _roll_no, 
+        string memory _date
+        ) public pure returns (string memory){
+        
+        bytes memory roll_no = bytes(_roll_no);
+        bytes memory date = bytes(_date);
+    
+        string memory combine = new string(roll_no.length + date.length);
+        bytes memory babcde = bytes(combine);
+        uint k = 0;
+        for (uint i = 0; i < roll_no.length; i++) babcde[k++] = roll_no[i];
+        for (uint i = 0; i < date.length; i++) babcde[k++] = date[i];
+        return string(babcde);
+    }
+    
+    function generateCertificate(
+        string memory _organisation, 
+        string memory _name, 
+        string memory _course, 
+        string memory _score, 
+        string memory _date, 
+        string memory _roll_no
+        ) public returns(bytes32){
+        
+        bytes32 hash_id = sha256(abi.encodePacked(strConcat(_roll_no, _date))); 
+        hash = hash_id;
+        certificates[hash_id] = certificate( _organisation, _name, _course, _score, _date, _roll_no );
+        return hash_id;
+    }
+    
     function personID() public view returns(bytes32) {
         return hash;
     }
-
-    function generateCertificate(string memory _date, string memory _time, string memory _pName, string memory _pBirth, string memory addr) public returns(bytes32){
-        
-        bytes32 hash_id = sha256(abi.encodePacked(strConcat(_date, _pName))); 
-        hash = hash_id;
-        certificates[hash_id] = Certificate( _date, _time, _pName, _pBirth, addr);
-    }
     
-    function getData(bytes32 _id) public view returns(string memory, string memory, string memory, string memory, string memory) {
+    function getData(
+        bytes32 _id
+        ) public view returns(string memory, string memory, string memory, string memory, string memory, string memory) {
 
-        Certificate memory temp = certificates[_id];
+        certificate memory temp = certificates[_id];
 
-        return (temp.date, temp.time, temp.pName, temp.place, temp.addr);
+        return (temp.organisation, temp.name, temp.course, temp.score, temp.date, temp.roll_no);
     }
     
 }
